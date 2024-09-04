090bf5b2b81b939275f1c887abbdf72cd5402712
Himanshu kotia <himanshukotia9@gmail.com>
2024-09-04 12:58:02 +0530

    sidebar component completed

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { GoHome, GoHistory } from "react-icons/go";
+import { SiYoutubeshorts, SiYoutubemusic } from "react-icons/si";
+import { MdOutlineSubscriptions, MdOutlineWatchLater, MdPlaylistPlay } from "react-icons/md";
+import { BiLike } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+
+export default function Sidebar() {
+    const mainLinks = [
+        {icon: <GoHome className='text-xl'/>, title: "Home" },
+        {icon: <SiYoutubeshorts className='text-xl'/>, title: "Shorts"},
+        {icon: <MdOutlineSubscriptions className='text-xl'/>, title: "Subscriptions"},
+        {icon: <SiYoutubemusic className='text-xl'/>, title: "Youtube Music"},
+    ];
+
+    const otherLinks = [
+        {icon: <CgProfile className='text-xl'/>, title: "Your channel"},
+        {icon: <GoHistory className='text-xl'/>, title: "History" },
+        {icon: <MdPlaylistPlay className='text-xl'/>, title: "Playlists"},
+        {icon: <MdOutlineWatchLater className='text-xl'/>, title: "Watch later"},
+        {icon: <BiLike className='text-xl'/>, title: "Liked videos"},
+
+    ];
+
+
+  return (
+    <div className='w-1/3 md:w-1/6 bg-[#212121] px-5 overflow-auto pb-8 h-screen'>
+        <ul className='flex flex-col border-b  border-gray-600 py-2'>
+            {mainLinks.map(({icon, title}) => {
+                return(
+                    <li key={title}>
+                        <a href="#" className="flex items-center gap-4 pl-2 py-3 hover:bg-zinc-600 rounded-xl">{icon}<span className='text-sm tracking-wider'>{title}</span></a>
+                    </li>
+                )
+            })}
+        </ul>
+        <ul className='flex flex-col border-b  border-gray-600 py-2'>
+            {otherLinks.map(({icon, title}) => {
+                return(
+                    <li key={title}>
+                        <a href="#" className="flex items-center gap-4 pl-2 py-3 hover:bg-zinc-600 rounded-xl">{icon}<span className='text-sm tracking-wider'>{title}</span></a>
+                    </li>
+                )
+            })}
+        </ul>
+    </div>
+  )
+}
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Sidebar from '../Components/Sidebar'
 
 export default function Homepage() {
   return (
     <div>
       <Navbar/>
+      <Sidebar/>
     </div>
   )
 }
